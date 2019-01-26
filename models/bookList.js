e7ce2a86031597b08b4ce42085e516c86f601beb
Rebeca Cabañas Arauz <beckyarauz@users.noreply.github.com>
2019-01-26 17:19:17 +0100

    saved starred books to BookList

--- models/bookList.js
+++ models/bookList.js
@@ -3,7 +3,7 @@ const Schema   = mongoose.Schema;
 
 const bookListSchema = new Schema({
   userId: mongoose.Schema.Types.ObjectId,   //will contain reference (_id) to stored book in "books" collection
-  bookId: mongoose.Schema.Types.ObjectId,   //will contain reference (_id) to stored user in "users" collection
+  bookId: String,   //will contain reference (_id) to stored user in "users" collection
   starred: Boolean                          //true if user has starred/favourited this book
 });
 
