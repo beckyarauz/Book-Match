251b5d64a72d064f4e938a72c7e5d5b95576510d
Rebeca Cabañas Arauz <beckyarauz@users.noreply.github.com>
2019-01-29 21:43:49 +0100

    added starred books on profile view and modified /profile route

--- routes/site.js
+++ routes/site.js
@@ -2,6 +2,8 @@ const express = require("express");
 const site = express.Router();
 const ensureLogin = require('connect-ensure-login');
 const request = require('request');
+const rp = require('request-promise');
+const fetch = require("node-fetch");
 const checkGuest = checkRoles('GUEST');
 const checkEditor = checkRoles('EDITOR');
 const checkAdmin = checkRoles('ADMIN');
@@ -21,68 +23,123 @@ site.get("/home", (req, res, next) => {
 });
 
 // //without params: render profile page for logged in user
-site.get('/profile',ensureLogin.ensureLoggedIn('/login'), (req, res) => {
+site.get('/profile', ensureLogin.ensureLoggedIn('/login'), (req, res) => {
   
-  User.findOne({username:req.user.username})
-  .then(user => {
+  let user,
+  username,
+    firstname,
+    lastname,
+    userpicture,
+    usercountry,
+    usercity,
+    userfbID,
+    userigID,
+    userslackID,
+    usertwitterID,
+    isProfileOwner,
+    bookList,
+    bookArr = [];
+
+
+  const getUser = async () => {
+    user = await User.findOne({
+        username: req.user.username
+      });
+    username = user.username;
+    firstname = user.firstName;
+    lastname = user.lastName;
+    userpicture = user.picture;
+    usercountry = user.country;
+    usercity = user.city;
+    userfbID = user.fbID;
+    userigID = user.igID;
+    userslackID = user.slackID;
+    usertwitterID = user.twitterID;
+    isProfileOwner = true;
+
+    bookList = await BookList.find({
+            userId: user._id
+    });
+    for(book of bookList){
+      // console.log('mybookList',bookList);
+      // console.log('mybook',book);
+      let url = `https://www.googleapis.com/books/v1/volumes/${book.bookId}?key=${process.env.GOOGLE_BOOKS_API_KEY}`;
+      // console.log(url);
+      const response = await fetch(url);
+      const json = await response.json();
+
+      bookArr.push(json.volumeInfo);
+      // console.log('bookArr', bookArr);
+    }
+  }
+ 
+  const getAllInfo = async () => {
+    await getUser();
     res.render('profile',{
-      username: user.username,
-      firstname: user.firstName,
-      lastname: user.lastName,
-      userpicture:user.picture,
-      usercountry: user.country,
-      usercity: user.city,
-      userfbID: user.fbID,
-      userigID: user.igID,
-      userslackID: user.slackID,
-      usertwitterID: user.twitterID,
-      isProfileOwner:true});
-  })
-  .catch(err => console.log(err));
-    
+      username, 
+      books: bookArr,firstname,
+      lastname,
+      userpicture,
+      usercountry,
+      usercity,
+      userfbID,
+      userigID,
+      userslackID,
+      usertwitterID,
+      isProfileOwner,
+      bookList,});
+  }
+
+  getAllInfo();
+
 });
 
 // //with params: render profile page for the user listed in the request
-site.get('/profile/:username',ensureLogin.ensureLoggedIn('/login'), (req, res) => {
-  
-  User.findOne({username:req.params.username})
-  .then(user => {
-    let isOwner = req.params.username==req.user.username;
-    res.render('profile',{
-      username: user.username,
-      firstname: user.firstName,
-      lastname: user.lastName,
-      userpicture:user.picture,
-      usercountry: user.country,
-      usercity: user.city,
-      userfbID: user.fbID,
-      userigID: user.igID,
-      userslackID: user.slackID,
-      usertwitterID: user.twitterID,
-      isProfileOwner:isOwner});
-  })
-  .catch(err => console.log(err));
-    
+site.get('/profile/:username', ensureLogin.ensureLoggedIn('/login'), (req, res) => {
+
+  User.findOne({
+      username: req.params.username
+    })
+    .then(user => {
+      let isOwner = req.params.username == req.user.username;
+      res.render('profile', {
+        username: user.username,
+        firstname: user.firstName,
+        lastname: user.lastName,
+        userpicture: user.picture,
+        usercountry: user.country,
+        usercity: user.city,
+        userfbID: user.fbID,
+        userigID: user.igID,
+        userslackID: user.slackID,
+        usertwitterID: user.twitterID,
+        isProfileOwner: isOwner
+      });
+    })
+    .catch(err => console.log(err));
+
 });
 
 site.get('/profile-setup', ensureLogin.ensureLoggedIn('/login'), (req, res) => {
-  User.findOne({username:req.user.username})
-  .then(user => {
-    res.render('profileSetup',{
-      username: user.username,
-      firstname: user.firstName,
-      lastname: user.lastName,
-      userpicture:user.picture,
-      description:user.description,
-      usercountry: user.country,
-      usercity: user.city,
-      userfbID: user.fbID,
-      userigID: user.igID,
-      userslackID: user.slackID,
-      usertwitterID: user.twitterID
-    });
-  })
-  .catch(err => console.log(err));
+  User.findOne({
+      username: req.user.username
+    })
+    .then(user => {
+      res.render('profileSetup', {
+        username: user.username,
+        firstname: user.firstName,
+        lastname: user.lastName,
+        userpicture: user.picture,
+        description: user.description,
+        usercountry: user.country,
+        usercity: user.city,
+        userfbID: user.fbID,
+        userigID: user.igID,
+        userslackID: user.slackID,
+        usertwitterID: user.twitterID
+      });
+    })
+    .catch(err => console.log(err));
 });
 
 site.post('/profile-setup', ensureLogin.ensureLoggedIn('/login'), (req, res) => {
@@ -101,42 +158,48 @@ site.post('/profile-setup', ensureLogin.ensureLoggedIn('/login'), (req, res) => 
     password: req.body.newpass
   };
   //console.log(`form data: ${updatedUser.firstname} ${updatedUser.lastname} ${updatedUser.description}`);
-  User.findOne({username:updatedUser.username})
-  .then(user => {
-    console.log(`form data: ${updatedUser.firstname} ${updatedUser.lastname} ${updatedUser.description}`);
-    if (updatedUser.password === "") {updatedUser.password = user.password;}
-    else {
-      let salt = bcrypt.genSaltSync(bcryptSalt);
-      updatedUser.password = bcrypt.hashSync(updatedUser.password, salt);
-      console.log('Update password. New hash: '+updatedUser.password);
-    }
-    
-    user.set({
-      firstName: updatedUser.firstname,
-      lastName: updatedUser.lastname,
-      picture: updatedUser.userpicture,
-      description: updatedUser.description,
-      country: updatedUser.country,
-      city: updatedUser.city,
-      fbID: updatedUser.fbID,
-      igID: updatedUser.igID,
-      slackID: updatedUser.slackID,
-      twitterID: updatedUser.twitterID,
-      password: updatedUser.password
-    });
-    user.save().then(user => {
-      //console.log(user);
-      res.redirect('/profile-setup');
+  User.findOne({
+      username: updatedUser.username
     })
-    .catch(err => {console.log(err)});
-  })
-  .catch(err => console.log(err));
+    .then(user => {
+      console.log(`form data: ${updatedUser.firstname} ${updatedUser.lastname} ${updatedUser.description}`);
+      if (updatedUser.password === "") {
+        updatedUser.password = user.password;
+      } else {
+        let salt = bcrypt.genSaltSync(bcryptSalt);
+        updatedUser.password = bcrypt.hashSync(updatedUser.password, salt);
+      }
+
+      user.set({
+        firstName: updatedUser.firstname,
+        lastName: updatedUser.lastname,
+        picture: updatedUser.userpicture,
+        description: updatedUser.description,
+        country: updatedUser.country,
+        city: updatedUser.city,
+        fbID: updatedUser.fbID,
+        igID: updatedUser.igID,
+        slackID: updatedUser.slackID,
+        twitterID: updatedUser.twitterID,
+        password: updatedUser.password
+      });
+      user.save().then(user => {
+          //console.log(user);
+          res.redirect('/profile-setup');
+        })
+        .catch(err => {
+          console.log(err)
+        });
+    })
+    .catch(err => console.log(err));
 
 });
 
 
 site.get('/admin', checkAdmin, (req, res) => {
-  res.render('admin', { user: req.user });
+  res.render('admin', {
+    user: req.user
+  });
 });
 
 site.get('/search', (req, res, next) => {
@@ -148,7 +211,9 @@ site.get('/search', (req, res, next) => {
       if (!error && response.statusCode == 200) {
         let info = JSON.parse(body);
         items = info.items.map(item => item);
-        res.render('search', { items });
+        res.render('search', {
+          items
+        });
         return;
       }
     })
@@ -157,21 +222,25 @@ site.get('/search', (req, res, next) => {
   }
 });
 
-site.post('/search',ensureLogin.ensureLoggedIn('/login'), (req, res, next)=>{
+site.post('/search', ensureLogin.ensureLoggedIn('/login'), (req, res, next) => {
   const action = req.body.action;
-  if(action.starred){
+  if (action.starred) {
     console.log('YEAH');
-    BookList.findOne({'bookId': action.id})
-    .then(data =>{
-      if(data === null){
-        User.findOne({username: req.user.username})
-        .then( user => {
-          createBookList(user._id, action.book, action.starred);
-        })
-      }
-    })
-    .catch(e => console.log(e));
-  } 
+    BookList.findOne({
+        'bookId': action.id
+      })
+      .then(data => {
+        if (data === null) {
+          User.findOne({
+              username: req.user.username
+            })
+            .then(user => {
+              createBookList(user._id, action.book, action.starred);
+            })
+        }
+      })
+      .catch(e => console.log(e));
+  }
 });
 
 
@@ -185,18 +254,18 @@ function checkRoles(role) {
   }
 }
 
-function createBookList(userId, bookId, starred){
-  bookList = new BookList ({
-    userId: userId,  
-    bookId: bookId, 
+function createBookList(userId, bookId, starred) {
+  bookList = new BookList({
+    userId: userId,
+    bookId: bookId,
     starred: starred
   })
 
   bookList.save()
-  .then(book =>{
-    console.log('Book was saved!');
-  })
-  .catch(e => console.log(e));
+    .then(book => {
+      console.log('Book was saved!');
+    })
+    .catch(e => console.log(e));
 }
 
 
