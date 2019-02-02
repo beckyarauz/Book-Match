ba3101b661093999b57720ce4fd89b96ffd1433f
Christian Standke <c.standke@gmail.com>
2019-02-02 11:13:41 +0100

    heroku setup changes

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const User = require('./models/user');
 // Mongoose configuration
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/book-match', { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
