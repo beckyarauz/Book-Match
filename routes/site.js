2264fb7a3a4d430d4ea22544b51217b70acdf677
Rebeca Cabañas Arauz <beckyarauz@users.noreply.github.com>
2019-02-06 16:30:54 +0100

    booklist validation

--- routes/site.js
+++ routes/site.js
@@ -29,73 +29,88 @@ function checkRoles(role) {
 // getUser function gets all the information needed to be rendered on user profiles
 const getUser = async (req,username) => {
   let user;
+  try{
 
-  ownBookList = await BookList.find({
-    userId: req.user._id,
-  });
 
-  ownBookList = ownBookList.map((el) => el.bookId);
-  console.log(ownBookList);
-  user = await User.findOne({
-    username: username
-  });
-  booklist = await BookList.find({
-    userId: user._id,
-  }); 
+    ownBookList = await BookList.find({
+      userId: req.user._id,
+    });
+  
+    if(ownBookList != null) {
+      ownBookList = ownBookList.map((el) => el.bookId);
+      console.log(ownBookList);
+    }
 
-  numMatchingBooks = await BookList.aggregate([
-    {
-      $match: {
-        userId: {$eq: user._id} 
-      }
-    },
-    {
-      $group: { //calculate number matching books for each user
-        _id: '$userId',
-        matchingBooks: {
-          $sum: {
-            $cond: [{
-              $in: ["$bookId", ownBookList]
-            }, 1, 0]
+    user = await User.findOne({
+      username: username
+    });
+
+    booklist = await BookList.find({
+      userId: user._id,
+    }); 
+  
+    if(booklist !== null && booklist !== undefined && booklist.length > 0){
+      numMatchingBooks = await BookList.aggregate([
+        {
+          $match: {
+            userId: {$eq: user._id} 
+          }
+        },
+        {
+          $group: { //calculate number matching books for each user
+            _id: '$userId',
+            matchingBooks: {
+              $sum: {
+                $cond: [{
+                  $in: ["$bookId", ownBookList]
+                }, 1, 0]
+              }
+            }
           }
         }
+      ])
+    
+      numMatchingBooks = numMatchingBooks[0].matchingBooks;
+    }
+    
+    // console.log(numMatchingBooks);
+  
+    let userInfo = {
+      username : user.username,
+      firstname : user.firstName,
+      lastname : user.lastName,
+      gender : user.gender,
+      userpicture : user.picture,
+      usercountry : user.country,
+      usercity : user.city,
+      userfbID : user.fbID,
+      userigID : user.igID,
+      userslackID : user.slackID,
+      usertwitterID : user.twitterID,
+      isProfileOwner : req.params.username == req.user.username,
+      bookList: booklist,
+      bookArr : [],
+      favBookArr : [],
+      numMatchingBooks: numMatchingBooks || undefined
+    }
+    for (book of userInfo.bookList) {
+      let url = `https://www.googleapis.com/books/v1/volumes/${book.bookId}?key=${process.env.GOOGLE_BOOKS_API_KEY}`;
+      const response = await fetch(url);
+      const json = await response.json();
+      if(json.error){
+        console.log('json',json.error.message);
+      }
+      userInfo.bookArr.push(json.volumeInfo);
+  
+      if (book.starred) {
+        userInfo.favBookArr.push(json.volumeInfo);
       }
     }
-  ])
-
-  numMatchingBooks=numMatchingBooks[0].matchingBooks
-  // console.log(numMatchingBooks);
-
-  let userInfo = {
-    username : user.username,
-    firstname : user.firstName,
-    lastname : user.lastName,
-    gender : user.gender,
-    userpicture : user.picture,
-    usercountry : user.country,
-    usercity : user.city,
-    userfbID : user.fbID,
-    userigID : user.igID,
-    userslackID : user.slackID,
-    usertwitterID : user.twitterID,
-    isProfileOwner : req.params.username == req.user.username,
-    bookList: booklist,
-    bookArr : [],
-    favBookArr : [],
-    numMatchingBooks: numMatchingBooks
+    return userInfo;
+  } catch(e){
+    console.log(e.message);
   }
-  for (book of userInfo.bookList) {
-    let url = `https://www.googleapis.com/books/v1/volumes/${book.bookId}?key=${process.env.GOOGLE_BOOKS_API_KEY}`;
-    const response = await fetch(url);
-    const json = await response.json();
-
-    userInfo.bookArr.push(json.volumeInfo);
-
-    if (book.starred) {
-      userInfo.favBookArr.push(json.volumeInfo);
-    }
-  }
-  return userInfo;
+  
 }
 
 const createBookList = async (userId, bookId, starred) => {
