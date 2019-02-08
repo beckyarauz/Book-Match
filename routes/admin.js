4d55efe62329176674695cf9fab3fee2f9ffe2e9
Rebeca Cabañas Arauz <beckyarauz@users.noreply.github.com>
2019-02-08 17:31:37 +0100

    admin manage users route created

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,57 @@
 const express = require('express');
 const admin = express.Router();
-const ensureLogin = require('connect-ensure-login'); 
+const ensureLogin = require('connect-ensure-login');
 
-admin.get('/',ensureLogin.ensureLoggedIn('/login'),(req, res, next) =>{
+const User = require('../models/user');
+const Book = require('../models/book');
+const BookList = require('../models/bookList');
+const Message = require('../models/message');
+
+let checkAdmin = function (req, res, next) {
     let role = 'ADMIN';
     if (req.user.role === role) {
-        res.render('admin', {
-            layout:'private-layout',
-            user: req.user
-          });
+        next();
     } else {
-      res.redirect('/home');
+        res.redirect('/home');
     }
+}
+
+admin.use(ensureLogin.ensureLoggedIn('/login'));
+admin.use(checkAdmin);
+
+admin.get('/', (req, res, next) => {
+    res.render('admin', {
+        layout: 'private-layout',
+        admin: req.user
+    });
+});
+
+admin.get('/manage-users', (req, res, next) => {
+    let notfound = 'No users found';
+    (async () => {
+        try {
+            let users = await User.find();
+
+            if (users !== null) {
+                res.render('manage-users', {
+                    layout: 'private-layout',
+                    admin: req.user,
+                    users
+                });
+            } else {
+                res.render('manage-users', {
+                    layout: 'private-layout',
+                    admin: req.user,
+                    error: notfound
+                });
+            }
+        } catch (e) {
+            console.log('GET /manage-users Error:', e.message)
+        }
+
+    })();
 })
+
+
 
 module.exports = admin;
