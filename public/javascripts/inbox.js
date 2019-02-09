a46efe0e6883333100c9d4d3198604ca13a76a86
Rebeca Cabañas Arauz <beckyarauz@users.noreply.github.com>
2019-02-09 02:50:51 +0100

    feature: delete messages, updated inbox view

---
+++ public/javascripts/inbox.js
@@ -0,0 +1,56 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    $('.sent-mail').toggle();
+    
+    $('.delete-mail').click(function () {
+        let mailId = $(this).attr('data-mail');
+
+        console.log(mailId);
+        let confirmDelete = confirm('Are you sure you want to delete this message?');
+        if (confirmDelete) {
+            axios.post('/inbox', {
+                action: {
+                    delete: true,
+                    many: false,
+                    messageId: mailId
+                }
+            })
+            $(this).parent().parent().toggle();
+        }
+    });
+
+    $('#delete-all-mail').click(function () {
+
+        let checkedMailIds = [];
+        let checkedMail = $("input:checked");
+        for (mail of checkedMail) {
+            checkedMailIds.push(mail.dataset.mail);
+        }
+        if (checkedMailIds.length > 0) {
+            let confirmDelete = confirm('Are you sure you want to delete these messages?');
+            if (confirmDelete) {
+                axios.post('/inbox', {
+                    action: {
+                        delete: true,
+                        many: true,
+                        messageIds: checkedMailIds
+                    }
+                })
+                for (mail of checkedMail) {
+                    mail.parentElement.parentElement.hidden = true;
+                }
+            }
+        } else {
+            alert(`You haven't selected any messages to delete`);
+        }
+    });
+
+    $('#sent-mail').click(function (){
+        $('.sent-mail').show();
+        $('#inbox-mail').hide();
+    })
+    $('#in-mail').click(function (){
+        $('.sent-mail').hide();
+        $('#inbox-mail').show();
+    })
+})
